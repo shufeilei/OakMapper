b3083b883aa9cf64ccac66d493efa76998a363ea
Shufei Lei <shufeilei@berkeley.edu>
2013-03-21 16:38:52 -0700

    Initial Commit

---
+++ OakMapper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OakMapper
+//
+//  Created by Shufei Lei on 3/21/13.
+//  Copyright (c) 2013 UCB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OakMapperAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OakMapperAppDelegate class]));
+    }
+}
